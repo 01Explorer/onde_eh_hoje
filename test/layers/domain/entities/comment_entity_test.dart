81114ee15c5edc2bb61ce4e4473049856dbe7454
Gabriel Santos <gabrielsantostca@gmail.com>
2023-01-31 23:32:06 -0300

    refactor(comment_entity): made the Id nullable since it's created on the DB

--- test/layers/domain/entities/comment_entity_test.dart
+++ test/layers/domain/entities/comment_entity_test.dart
@@ -6,17 +6,29 @@ import '../../../mocks/mocks.dart';
 
 void main() {
   group('Comment Entity', () {
-    test('When created should return a commenti entity with the desired inputs',
+    test('When created should return a comment entity with the desired inputs',
         () {
       final Comment commentTest = mockedComment;
 
-      expect(commentTest.id, 0);
-      expect(commentTest.id, isA<int>());
+      expect(commentTest.id, "75d11f00-7ebe-4c8e-8fdd-6662db353d9c");
+      expect(commentTest.id, isA<String>());
       expect(commentTest.user.name, mockedUser.name);
       expect(commentTest.user, isA<User>());
       expect(commentTest.createdAt, mockedComment.createdAt);
       expect(commentTest.createdAt, isA<DateTime>());
       expect(commentTest.message, mockedComment.message);
+      expect(commentTest.message, isA<String>());
+    });
+    test('The entity can be created without passing a id since it\'s nullable',
+        () {
+      final Comment commentTest = mockedNullIdComment;
+
+      expect(commentTest.id, isNull);
+      expect(commentTest.user.name, mockedUser.name);
+      expect(commentTest.user, isA<User>());
+      expect(commentTest.createdAt, mockedNullIdComment.createdAt);
+      expect(commentTest.createdAt, isA<DateTime>());
+      expect(commentTest.message, mockedNullIdComment.message);
       expect(commentTest.message, isA<String>());
     });
   });
--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -2,14 +2,24 @@ import 'package:onde_eh_hoje/layers/domain/entities/comment_entity.dart';
 import 'package:onde_eh_hoje/layers/domain/entities/place_entity.dart';
 import 'package:onde_eh_hoje/layers/domain/entities/user_entity.dart';
 
-final User mockedUser =
-    User(id: 0, name: 'Gabriel', email: 'gabjhsa@gmail.com');
+final User mockedUser = User(
+    id: "75d11f00-7ebe-4c8e-8fdd-6662db353d9c",
+    name: 'Gabriel',
+    email: 'gabjhsa@gmail.com');
 
 final Place mockedPlace = Place(
-    id: 0, name: 'Legend', latitude: -59.87432174921, longitude: 60.8320948);
+    id: "75d11f00-7ebe-4c8e-8fdd-6662db353d9c",
+    name: 'Legend',
+    latitude: -59.87432174921,
+    longitude: 60.8320948);
 
 final Comment mockedComment = Comment(
-    id: 0,
+    id: "75d11f00-7ebe-4c8e-8fdd-6662db353d9c",
+    user: mockedUser,
+    message: 'This is a sample message',
+    place: mockedPlace);
+
+final Comment mockedNullIdComment = Comment(
     user: mockedUser,
     message: 'This is a sample message',
     place: mockedPlace);
