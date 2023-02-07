da0c1325e15e8b2120cc31bf434c39b612aefa0c
Gabriel Santos <gabrielsantostca@gmail.com>
2023-02-07 15:38:34 -0300

    feat(http_service): developed the interface to the http_service

--- lib/layers/domain/services/http_service/http_service.dart
+++ lib/layers/domain/services/http_service/http_service.dart
@@ -4,10 +4,12 @@ abstract class HttpService {
   Future<Response<T>> get<T>(String path,
       {Map<String, dynamic>? queryParameters});
 
-  Future<Response<T>> post<T>(String path, {Map<String, dynamic> data});
+  Future<Response<T>> post<T>(String path,
+      {required Map<String, dynamic> data});
 
   Future<Response<T>> put<T>(String path,
-      {Map<String, dynamic> data, Map<String, dynamic> queryParameters});
+      {required Map<String, dynamic> data,
+      required Map<String, dynamic> queryParameters});
 
   Future<Response<T>> delete<T>(String path,
       {Map<String, dynamic>? queryParameters});
