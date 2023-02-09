9a31e3fb43ee8663b0b855cfa6b3980bdc7ff768
Gabriel Santos <gabrielsantostca@gmail.com>
2023-02-09 14:48:48 -0300

    feat(utils): added places api endpoints

--- lib/layers/domain/utils/utils.dart
+++ lib/layers/domain/utils/utils.dart
@@ -3,4 +3,8 @@ class Utils {
   static const String getUserApiEndpoint = 'https://test-url/api/get';
   static const String putUserApiEndpoint = 'https://test-url/api/put';
   static const String postUserApiEndpoint = 'https://test-url/api/post';
+  static const String deletePlaceApiEndpoint = 'https://test-url/api/deletePlace';
+  static const String getPlaceApiEndpoint = 'https://test-url/api/post';
+  static const String putPlaceApiEndpoint = 'https://test-url/api/post';
+  static const String postPlaceApiEndpoint = 'https://test-url/api/post';
 }
