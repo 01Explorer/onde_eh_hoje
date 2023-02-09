32e3f3da9b2475a2cfba981e18eaa01496e9d0fd
Gabriel Santos <gabrielsantostca@gmail.com>
2023-02-09 14:17:39 -0300

    feat(utils): added the user datasource api endpoints

---
+++ lib/layers/domain/utils/utils.dart
@@ -0,0 +1,6 @@
+class Utils {
+  static const String deleteUserApiEndpoint = 'https://test-url/api/delete';
+  static const String getUserApiEndpoint = 'https://test-url/api/get';
+  static const String putUserApiEndpoint = 'https://test-url/api/put';
+  static const String postUserApiEndpoint = 'https://test-url/api/post';
+}
