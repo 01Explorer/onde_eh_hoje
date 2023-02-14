131b88957512bf9b05dd1669983eb1c7b566ce27
Gabriel Santos <gabrielsantostca@gmail.com>
2023-02-14 10:46:33 -0300

    feat(utils): added more api endpoints

--- lib/layers/domain/utils/utils.dart
+++ lib/layers/domain/utils/utils.dart
@@ -3,8 +3,20 @@ class Utils {
   static const String getUserApiEndpoint = 'https://test-url/api/get';
   static const String putUserApiEndpoint = 'https://test-url/api/put';
   static const String postUserApiEndpoint = 'https://test-url/api/post';
-  static const String deletePlaceApiEndpoint = 'https://test-url/api/deletePlace';
+  static const String deletePlaceApiEndpoint =
+      'https://test-url/api/deletePlace';
   static const String getPlaceApiEndpoint = 'https://test-url/api/post';
   static const String putPlaceApiEndpoint = 'https://test-url/api/post';
   static const String postPlaceApiEndpoint = 'https://test-url/api/post';
+  static const String getCommentApiEndpoint = 'https://test-url/api/comment';
+  static const String getAllCommentApiEndpoint =
+      'https://test-url/api/comments';
+  static const String getCommentByPlaceApiEndpoint =
+      'https://test-url/api/commentByPlace';
+  static const String getCommentByUserApiEndpoint =
+      'https://test-url/api/commentByUser';
+  static const String deleteCommentApiEndpoint =
+      'https://test-url/api/deleteComment';
+  static const String postCommentApiEndpoint =
+      'https://test-url/api/postComment';
 }
